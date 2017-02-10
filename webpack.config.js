de31c44730833273c98497a35009eb58fa7462cf
hungpham-pyco <hung.phamtan@pyramid-consulting.com>
2017-02-10 13:24:21 +0700

    add HRM for webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,36 @@ var config = {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015','react']
-      }
+      loader: 'babel'
     }]
+  },
+	devServer: {
+		port: 8001,
+    contentBase: "./public",
+    colors: true,
+    historyApiFallback: true,
+    inline: true,
+		hot: true
   }
+}
+
+
+/*
+ * If bundling for production, optimize output
+ */
+if (process.env.NODE_ENV === 'production') {
+  config.devtool = false;
+  config.plugins = [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({comments: false}),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: JSON.stringify('production')}
+    })
+  ];
+} else {
+	config.plugins = [
+    new webpack.HotModuleReplacementPlugin()
+  ];
 }
 
 module.exports = config;
